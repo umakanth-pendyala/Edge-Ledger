45ae65f6df2b53cdadb8782cd621a5bbb195b506
umakanth pendyala <umakanthpendyala@gmail.com>
2020-08-05 20:06:17 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 14,
+//   });
+
+//   var marker = new google.maps.Marker({
+//     position: { lat: -34.397, lng: 150.644 },
+//     map: map,
+//   });
+// }
+
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash != "") {
+    // alert("i am in loop");
+    event.preventDefault();
+    const hash = this.hash;
+    console.log(hash);
+
+    $("html", "body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
